02ab9461fdcee7f7c33000ec2ce1021bb9d91d08
ChaTbiM <chatbimdev@gmail.com>
2019-07-29 10:44:08 -0700

    ths,solution is not well accepted in the community

--- Day-23/app.js
+++ Day-23/app.js
@@ -20,18 +20,41 @@
 
 // Dont Repeat Yourself ! 
 
-function createElf(name,weapon){
-    return {
-        name,
-        weapon,
-        attack(){
-            return ("attack with ") + weapon;
-        }
+// function createElf(name,weapon){
+//     return {
+//         name,
+//         weapon,
+//         attack(){
+//             return ("attack with ") + weapon;
+//         }
+//     }
+// }
+
+// const Orwell = createElf("Orwell","bow");
+// const Sally = createElf("Sally","Bazuka");
+
+// console.log(Orwell);
+// console.log(Sally);
+
+// Because of the attack method is repeated it takes alot of memory 
+
+// Another solution with prototypal inheritance
+// Object.create()
+
+const elfFunctions = {
+    attack(){
+        return ("attack with " + this.weapon);
     }
+};
+
+function createElf(name,weapon){
+    let newElf = Object.create(elfFunctions);
+    newElf.name = name;
+    newElf.weapon = weapon;
+
+    return newElf;
 }
 
-const Orwell = createElf("Orwell","bow");
-const Sally = createElf("Sally","Bazuka");
+const Orwell = createElf("Orwell","Arrows");
+const Peter = createElf("Peter", "Stones");
 
-console.log(Orwell);
-console.log(Sally);
