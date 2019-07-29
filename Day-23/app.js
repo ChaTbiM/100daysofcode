db847152a15a4b305f020adcd7c88bef8c208a74
ChaTbiM <chatbimdev@gmail.com>
2019-07-29 10:27:28 -0700

    factory functions have problem of memory

--- Day-23/app.js
+++ Day-23/app.js
@@ -1,19 +1,37 @@
 
-const elf = {
-    name:"Orwell",
-    weapon: "bow",
-    attack(){
-        console.log("attack with a bow")
+// const elf = {
+//     name:"Orwell",
+//     weapon: "bow",
+//     attack(){
+//         console.log("attack with a bow")
+//     }
+// }
+
+// const elf2 = {
+//     name:"Sally",
+//     weapon:"bow",
+//     attack(){
+//         console.log("attack with a bow!");
+//     }
+// }
+
+// console.log(elf);
+// console.log(elf2);
+
+// Dont Repeat Yourself ! 
+
+function createElf(name,weapon){
+    return {
+        name,
+        weapon,
+        attack(){
+            return ("attack with ") + weapon;
+        }
     }
 }
 
-const elf2 = {
-    name:"Sally",
-    weapon:"bow",
-    attack(){
-        console.log("attack with a bow!");
-    }
-}
+const Orwell = createElf("Orwell","bow");
+const Sally = createElf("Sally","Bazuka");
 
-console.log(elf);
-console.log(elf2);
+console.log(Orwell);
+console.log(Sally);
