40017b7a63b9b5772f1bb6040e1d07f8844efb8c
ChaTbiM <chatbimdev@gmail.com>
2019-07-29 10:17:14 -0700

    DRY! , sovle it with factory functions...

---
+++ Day-23/app.js
@@ -0,0 +1,19 @@
+
+const elf = {
+    name:"Orwell",
+    weapon: "bow",
+    attack(){
+        console.log("attack with a bow")
+    }
+}
+
+const elf2 = {
+    name:"Sally",
+    weapon:"bow",
+    attack(){
+        console.log("attack with a bow!");
+    }
+}
+
+console.log(elf);
+console.log(elf2);
