5b8127208f89f901898f4a950acdbfec0ee4e4ed
ChaTbiM <chatbimdev@gmail.com>
2019-07-31 03:11:10 -0700

    booleans and conditionals

--- day-25/php/index.php
+++ day-25/php/index.php
@@ -1,10 +1,15 @@
 <?php
 
 
+// associative arrays
+// $task = [
+//     'title' => 'learning php',
+//     'due' => 'today',
+//     'assigned to' => 'me',
+//     'completed' => false,
+// ];
 
 
 
 
-
-
-require 'index.view.php';
+// require 'index.view.php';
--- day-25/php/index.view.php
+++ day-25/php/index.view.php
@@ -9,6 +9,30 @@
 <body>
 
 <h1>Second DAy Php</h1>
-    
+
+<h2>task for the day</h2>
+<ul>
+    <?php foreach($task as $key => $value) : ?>
+        <li> <strong><?=ucwords($key) ?>:</strong> <?= $value ?> </li>
+    <?php endforeach ; ?>
+
+    <li> <strong> Status : </strong> <?= $task['completed'] ? 'complete' : 'Incomplete' ; ?> </li>
+
+    <?php 
+        if($task['completed']){
+            echo 'complete';
+        }else {
+            echo 'incomplete';
+        }
+    ?>
+
+    <?php if($task['completed']) : ?>)
+        <div><span> &#9989 completed </span></div>
+    <?php elseif(! $task['completed']) : ?>
+        <div><span> incomplete......... </span></div>
+    <?php endif ;?>
+</ul>
+
+
 </body>
 </html>
