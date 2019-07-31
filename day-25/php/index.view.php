0606cbd31934ca18054410eae7023d218b42f8f7
ChaTbiM <chatbimdev@gmail.com>
2019-07-31 02:32:32 -0700

    Day 25 , init second day of php

---
+++ day-25/php/index.view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Second Day php</title>
+</head>
+<body>
+
+<h1>Second DAy Php</h1>
+    
+</body>
+</html>
