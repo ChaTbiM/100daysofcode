03a328f412e9f0c3cfbebfdc7888d61bb45c9724
ChaTbiM <chatbimdev@gmail.com>
2019-07-31 14:39:09 -0700

    Classes , objects ....!

--- day-25/php/index.php
+++ day-25/php/index.php
@@ -2,11 +2,42 @@
 
 require 'functions.php';
 
-$animals = ['dog','cat'] ;
+// $animals = ['dog','cat'] ;
 
 
+// dd(['horse','lion','car?']);
+
+// 
+
+class Task{
+
+    public $description;    
+    
+    protected $completed = false;
+
+    public function __construct($description){
+        $this->description = $description;
+    }
+
+    public function isComplete(){
+        return $this->completed;
+    }
+
+    public function complete(){
+        $this->completed = true;
+    }
+
+}
+
+$tasks = [
+    new Task('go to the store and buy...'),
+    new Task('learn php'),
+    new Task('learn Mysql'),
+    new Task('learn Laravel '),
+];
+
+$tasks[0]->complete();
+
+require 'classes.view.php';
 
 
-dd(['horse','lion','car?']);
-
-?>
